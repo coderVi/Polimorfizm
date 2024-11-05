3793ea9edb9c78a9096b12d720e74c15d77c79c5
coderVi <mr.asmtylmz@gmail.com>
2024-11-05 20:06:32 +0300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Polimorfizm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SnfTemel snfTemel = new SnfTemel();
+            snfTemel.mtdMesaj("Selam");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SnfTuremis snfTuremis = new SnfTuremis();
+            snfTuremis.mtdMesaj("Bu bir Poliforfizm örneğidir.");
+        }
+    }
+}
---
+++ SnfTemel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Polimorfizm
+{
+    internal class SnfTemel
+    {
+        public virtual void mtdMesaj(string icerik)
+        {
+            MessageBox.Show(icerik);
+        }
+    }
+
+    internal class SnfTuremis : SnfTemel
+    {
+        public override void mtdMesaj(string icerik)
+        {
+            MessageBox.Show(icerik.ToUpper());  //Türemiş olan ovirrede edilerek gösterilir
+        }
+    }
+}
